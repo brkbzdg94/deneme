8b3668e29fa4f6170991774fccd9062a65b8e5d9
Burak <42558581+brkbzdg94@users.noreply.github.com>
2018-08-25 17:25:38 +0300

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+        int sayi1, sayi2;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+        
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        sayi1 = Convert.ToInt32(TextBox1.Text);
+        sayi2 = Convert.ToInt32(TextBox2.Text);
+        int sonuc=topla(sayi1, sayi2);
+        Label1.Text = sonuc.ToString();
+    }
+    int topla(int s1,int s2)
+    {
+        int toplam;
+        toplam = s1 + s2;
+        return toplam; 
+    }
+}
